a62e3e4bd74ff4f35f7ace3e5bd81a262ff7dddb
maruiping <13833589337@163.com>
2020-05-18 15:29:14 +0800

    增加highcharts依赖，echarts应该也一样，option配置项要记得声明对象类型

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about" id="charts"></div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HighCharts from "highcharts";
+@Component({
+    name: "about"
+})
+export default class About extends Vue {
+    mounted() {
+        this.drawCharts();
+    }
+    drawCharts() {
+        let option: object = { // option声明类型 不然ts会报错
+            chart: {
+                type: "bar" //指定图表的类型，默认是折线图（line）
+            },
+            title: {
+                text: "我的第一个图表" // 标题
+            },
+            xAxis: {
+                categories: ["苹果", "香蕉", "橙子"] // x 轴分类
+            },
+            yAxis: {
+                title: {
+                    text: "吃水果个数" // y 轴标题
+                }
+            },
+            series: [
+                {
+                    // 数据列
+                    name: "小明", // 数据列名
+                    data: [1, 0, 4] // 数据
+                },
+                {
+                    name: "小红",
+                    data: [5, 7, 3]
+                }
+            ]
+        };
+        HighCharts.chart("charts", option);
+    }
+}
+</script>
