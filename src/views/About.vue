7cfbda5dde4a153250c2b6aa84e4e81d886b5b0b
maruiping <13833589337@163.com>
2020-05-18 15:34:12 +0800

    另一种方式声明绘制图表的option

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,8 @@ export default class About extends Vue {
         this.drawCharts();
     }
     drawCharts() {
-        let option: object = { // option声明类型 不然ts会报错
+        let option: object = {
+            // option声明类型 不然ts会报错
             chart: {
                 type: "bar" //指定图表的类型，默认是折线图（line）
             },
@@ -40,7 +41,33 @@ export default class About extends Vue {
                 }
             ]
         };
-        HighCharts.chart("charts", option);
+        HighCharts.chart("charts", {
+            chart: {
+                type: "bar" //指定图表的类型，默认是折线图（line）
+            },
+            title: {
+                text: "我的第一个图表" // 标题
+            },
+            xAxis: {
+                categories: ["苹果", "香蕉", "橙子"] // x 轴分类
+            },
+            yAxis: {
+                title: {
+                    text: "吃水果个数" // y 轴标题
+                }
+            },
+            series: [
+                {
+                    // 数据列
+                    name: "小明", // 数据列名
+                    data: [1, 0, 4] // 数据
+                },
+                {
+                    name: "小红",
+                    data: [5, 7, 3]
+                }
+            ]
+        } as object); // 或者 as object
     }
 }
 </script>
