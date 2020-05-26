e0b9f088e47a78dedf18e4f4f82c5219d12b6a24
maruiping <13833589337@163.com>
2020-05-26 14:25:11 +0800

    ...

--- src/assets/js/ajaxUrl.ts
+++ src/assets/js/ajaxUrl.ts
@@ -11,7 +11,7 @@ interface DataObj {
 }
 export function ajax(config: Config) {
     return new Promise((resolve: (value: string) => void, reject: (value: string) => void) => { // 定义返回值类型为字符型
-        let data = config.data || {},timeOut=Number(config.timeOut);
+        let data = config.data || {}, timeOut = Number(config.timeOut);
         config.type === config.type.toLocaleLowerCase()
         let urlData: string = Object.entries(data).map(([key, val]) => `${key}=${val}`).join("&")
         // entries 将对象转成可迭代类型数据 数组中包含键和值 {a:1,b:2} => [["a",1],["b",2]]
